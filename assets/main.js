3ef83b302da2c14d35a879e479850d2a9600e8b7
udberg <johnuduak066@gmail.com>
2021-04-13 13:13:00 +0100

    Fix linters errors

--- assets/main.js
+++ assets/main.js
@@ -13,36 +13,36 @@ function Book(author, title, pages, read) {
 }
 
 button.addEventListener('click', function() {
-   container.classList.contains('invisible') ? container.classList.remove('invisible') : 
-   container.classList.add('invisible');
-  });
+    container.classList.contains('invisible') ? container.classList.remove('invisible') 
+    : container.classList.add('invisible');
+    });
 
 form.addEventListener('submit', function() {
 
-  
+
   // Get form values
-  const author= document.querySelector('.author').value,
-  title = document.querySelector('.title').value,
-  pages = document.querySelector('.pages').value,
-  read = document.querySelector('#read')
+  const author = document.querySelector('.author').value,
+    title = document.querySelector('.title').value,
+    pages = document.querySelector('.pages').value,
+    read = document.querySelector('#read');
   let data;
 
   //Checkbox validation
-  read.checked ? data = read.value = 'YES' : data = read.value = "NO";
-  
+  read.checked ? data = read.value = 'YES' : data = read.value = 'NO';
+
   //Object Instantiation
   const book = new Book(author, title, pages, data);
-  
+
   library.push(book);
-  
+ 
   addBookToLibrary(library);
-  
+
 });
 
 
-function addBookToLibrary(input) {
+  function addBookToLibrary(input) {
   // Create tr element
-  const row = document.createElement('tr');
+    const row = document.createElement('tr');
  
   
 Object.values(input).forEach((book) => {
@@ -53,7 +53,7 @@ Object.values(input).forEach((book) => {
     <td> ${book.pages} </td>
     <td> ${book.read} </td>
     <td><a href="#" class="delete">X<a></td>'`;
-  
+
     list.appendChild(row);
 
   });
